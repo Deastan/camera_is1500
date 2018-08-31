83dbf29b90ac060cab464296ca77fb6145956e11
Jonathan <jonathanburkhard@gmail.com>
2018-08-31 08:37:39 +0200

    add the publisher in pointCloud (node the optimized type of message) Topic works ! Without right message.

--- src/camera_is1500_node.cpp
+++ src/camera_is1500_node.cpp
@@ -21,20 +21,50 @@
 
 int main(int argc, char **argv)
 {
+
+
+
+
+  ros::init(argc, argv, "interface_sf_ros_node");
+
+  // init. publisher
+  ros::NodeHandle nh;
+  ros::Publisher track_pub = nh.advertise<sensor_msgs::PointCloud>("track_camera", 50);
   // declaration of the variables
   std::vector<float> v;
-
-
   overInit();
-  ros::init(argc, argv, "interface_sf_ros_node");
-  // ros::NodeHandle nh;
-  // ros::Publisher track_pub = nh.advertise<sensor_msgs::PointCloud>("track_camera", 50);
-while(ros::ok())
-{
-  v = overGetData();
-  std::cout << v[0] << " " << v[1] << " " << v[2] << " "
-  << v[3] << " " << v[4] << " " << v[5] << std::endl;
-}
 
-return 0;
+  ros::Rate r(40);
+
+  while(nh.ok())
+  {
+    v = overGetData();
+    std::cout << v[0] << " " << v[1] << " " << v[2] << " "
+    << v[3] << " " << v[4] << " " << v[5] << std::endl;
+
+    sensor_msgs::PointCloud cloud;
+
+    cloud.header.stamp = ros::Time::now();
+    cloud.header.frame_id = "camera_is1500";
+
+    cloud.points.resize(1);
+    cloud.points[0].x = 5;//v[0];
+    cloud.points[0].y = 4;//v[1];
+    cloud.points[0].z = 3;//v[2];
+
+    // cloud.channels.resize(1);
+    // cloud.channels[0].name = "intensities";
+    // cloud.channels[0].values.resize(num_points);
+    // for(unsigned int j = 0; j < num_points; ++j){
+    //   cloud.points[j].x = ranges[j]*cos(angles[j]);
+    //   cloud.points[j].y = ranges[j]*sin(angles[j]);
+    //   cloud.points[j].z = 0; //to be handled by the laser transform frame
+    //   cloud.channels[0].values[j] = 100;
+    // } // end loop
+
+    track_pub.publish(cloud);
+    r.sleep();
+  } // end loop
+
+  return 0;
 } // end main
