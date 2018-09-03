36af0ef954d0ef8bc213df7215184c0b8867cff7
Jonathan <jonathanburkhard@gmail.com>
2018-09-03 08:59:37 +0200

    Try to I sfHub and script but doens't work. try to do that later

--- src/scriptSHub.bash
+++ src/scriptSHub.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cd /home/jonathan/Documents/Kyburz/IS-1500_2017d/Camera/Linux/sfHub
+#cd /home/jonathan/Documents/Kyburz/IS-1500_2017d/Camera/Linux/sfHub
+cd /home/jonathan/ros_catkin_kyb/src/camera_is1500/external_software/sfHub
 ./sfHub
