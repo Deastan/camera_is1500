41884bcb6505fd3f06abc631acc9bff75896b05e
Deastan <jonathan@jonathan-HP-ZBook-Studio-G3-ubuntu16.04>
2018-10-18 14:23:23 +0200

    improve a bit

--- src/waypoint_node.py
+++ src/waypoint_node.py
@@ -1,42 +1,49 @@
 #! /usr/bin/env python
 
-#Basic imports
+
+# ET1 controller with camera_is1500
+# Jonathan Burkhard, Kyburz 2018
+# Documentations : Not exist yet
+#
+
+# TODO
+# - Add gain
+# - Clean the code with function, private variables etc...
+
+# Basic imports
 import sys
 import rospy
 import time
 import tf
 import numpy as np
-
+import math
+import tf.transformations
 
 from geometry_msgs.msg import Quaternion, Point, Pose, Twist, Vector3
 from std_msgs.msg import Empty, Float32
 from nav_msgs.msg import Odometry
 from math import radians, pow
-import tf.transformations
+
 # from std_msgs.msg import Empty
 from std_msgs.msg import String
-import math
-# Usefull function
+
+
+#*******************************************************************************
+#   Usefull functions
+#*******************************************************************************
+
 def dotproduct(v1, v2):
   return sum((a*b) for a, b in zip(v1, v2))
+
 def length(v):
   return math.sqrt(dotproduct(v, v))
-# error if angle = 0 or pi
-# def angle(v1, v2):
-#   return math.acos(dotproduct(v1, v2) / (length(v1) * length(v2)))
 
 def unit_vector(vector):
     """ Returns the unit vector of the vector.  """
     return vector / np.linalg.norm(vector)
 
-#def angle(v1, v2):
-#    """ Returns the angle in radians between vectors 'v1' and 'v2'::  """
-#    v1_u = unit_vector(v1)
-#    v2_u = unit_vector(v2)
-#    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
-
-#Publish directly what we need for the motor
+# Publish directly what we need for the motor
 global motor_pub
 motorpub = rospy.Publisher('/motor_state', String, queue_size=100)
 
@@ -48,19 +55,17 @@ global ready_to_go
 ready_to_go = rospy.set_param('/waypoint_node/ready_to_go', False)#
 global success
 success = rospy.set_param('/waypoint_node/success', False)#
-global target_x
-target_x = rospy.set_param('/waypoint_node/target_x', 1.0)
-global target_y
-target_y = rospy.set_param('/waypoint_node/target_y', -1.5)
+global target
+target = rospy.set_param('/waypoint_node/target', [3.7, -1.5])
 
 
 def turning_callback(msg):
-    #print("Enter in callback")
     # Re-assignement inside func
     global ready_to_go
     global success
     global target_x
     global target_y
+    global target
 
     global mot_msg
     mot_msg= Twist()
@@ -69,6 +74,7 @@ def turning_callback(msg):
     success = rospy.get_param('/waypoint_node/success')
     target_x = rospy.get_param('/waypoint_node/target_x')
     target_y = rospy.get_param('/waypoint_node/target_y')
+    target = rospy.get_param('/waypoint_node/target')
 
 
     robot_x = msg.pose.pose.position.x
@@ -78,7 +84,7 @@ def turning_callback(msg):
     if(ready_to_go == 1 and success == 0):
         #print("Enter first cond")
         u = [np.cos(yaw), np.sin(yaw)] # direction of the robot
-        v = [target_x -robot_x, target_y - robot_y] # vector btw robot and target
+        v = [target[0] -robot_x, target[1] - robot_y] # vector btw robot and target
         angle = np.arctan2(v[1], v[0])
         err_angle = angle - yaw#angle(u, v)
 
@@ -123,80 +129,7 @@ def turning_callback(msg):
         mot_msg.angular.z = 0.0
     mot_pub.publish(mot_msg)
 
-
-    #publish a twist command for the motor
-    # mot_msg.linear.x = 0.25
-    # mot_msg.linear.y = 0
-    # mot_msg.angular.z = 1
-    # mot_pub.publish(mot_msg)
-    #rospy.loginfo("Received a /cmd_vel message!")
-    #rospy.loginfo("Linear Components: [%f, %f, %f]"%(msg.linear.x, msg.linear.y, msg.linear.z))
-    #rospy.loginfo("Angular Components: [%f, %f, %f]"%(msg.angular.x, msg.angular.y, msg.angular.z))
-
-    # Do velocity processing here:
-    # Use the kinematics of your robot to map linear and angular velocities into motor commands
-    # for the physics based model:
-    #v_l = msg.linear.x - msg.angular.z*robot_width/2#left wheel request of velocity
-    #v_r = msg.linear.x + msg.angular.z*robot_width/2#right wheel request of velocity
-    # Then set your wheel speeds (using wheel_left and wheel_right as examples)
-    #set_left(v_l)
-    #set_right(v_r)
-    # if(state=="running" or state=="running_sonar_off" or state=="critical_obstacle"):
-    #     if(msg.angular.z>0):
-    #         leftTurn()
-    #         #rospy.loginfo("angular left")
-    #     elif(msg.angular.z<0):
-    #         rightTurn()
-    #         #rospy.loginfo("angular right")
-    #     else:
-	#     noTurn()
-
-
-
-
-
-# def main_timer_callback(event):
-#     global state
-#     global motor_state
-#     global sonar_off
-#     global red_state
-#     global green_state
-#     global greet_timer
-#     global start_time
-#     global mot_msg
-#     mot_msg= Twist()
-#     if(state=="ready" and motor_state=="off"):
-#         motor_on()
-#         if(motor_state=="on"): #retries motor on command
-#             if(sonar_off):
-#                 state="running_sonar_off"
-#             else:
-# 	        state="running"
-#         rospy.loginfo("motors commanded on, put in run state")
-#     elif(state=="ready" and motor_state=="on"): #catch case where code failed but motor is still on
-#         if(sonar_off):
-#             state="running_sonar_off"
-# 	    red_state=False
-#             green_state=True
-#         else:
-#             state="running"
-#             red_state=False
-#             green_state=True
-#     statepub.publish(state)
-#     motor_pub="motor state: " + motor_state
-#     motorpub.publish(motor_pub)
-#     #publish a twist command for the motor
-#     #mot_msg.linear.x = 0.25
-#     #mot_msg.linear.y = 0
-#     #mot_msg.angular.z = 1
-#     #mot_pub.publish(mot_msg)
-#     #add elapsed system time
-#     greet_timer+=time.time()-start_time
-#     start_time=time.time()
-
 def run():
-
-
     start_time = time.time()
     rospy.loginfo("Waypoint starting up")
 
@@ -216,6 +149,10 @@ def run():
     rospy.spin()
     # timer.shutdown()
 
+
+#*******************************************************************************
+#   Main
+#*******************************************************************************
 if __name__ == '__main__':
     try:
         run()
@@ -226,5 +163,5 @@ if __name__ == '__main__':
     mot_msg.angular.z = 0.0
     ready_to_go = False
     success = True
-    print("end")
     mot_pub.publish(mot_msg)
+    print("end")
