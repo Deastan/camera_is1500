d9afa82675f035b0a3483cdbecb1004c8109199f
Deastan <jonathanburkhard@gmail.com>
2018-12-04 08:52:48 +0100

    Clean the code

--- src/sevenSense_tf.py
+++ src/sevenSense_tf.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
 
-# ET1 controller with camera_is1500
+# Interface to get back the position of the robot from sevensSense A.G.
 # Jonathan Burkhard, Kyburz 2018
 # Documentations : Not exist yet
 
 # TODO
-# - Add gain
-# - Clean the code with function, private variables etc...
+# - Patch up the main
+# - Create object ... and do it more nicer
 
 # Basic imports
 import sys
@@ -14,92 +14,34 @@ import rospy
 import time
 import tf
 import numpy as np
-import math
 import tf.transformations
 from geometry_msgs.msg import Quaternion, Point, Pose, Twist, Vector3
-from std_msgs.msg import Empty, Float32
 from nav_msgs.msg import Odometry
 from math import radians, pow
-
-# from std_msgs.msg import Empty
-from std_msgs.msg import String
-
-#*******************************************************************************
-#   Usefull functions
-#*******************************************************************************
-def dotproduct(v1, v2):
-  return sum((a*b) for a, b in zip(v1, v2))
-
-def length(v):
-  return math.sqrt(dotproduct(v, v))
-
-def unit_vector(vector):
-    """ Returns the unit vector of the vector.  """
-    return vector / np.linalg.norm(vector)
-
-#global mot_pub
-# mot_pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 1) # 100 to 1
-
-# rate = 0
-#
-# def publisher_sevenSense(sevenSense_pub, listener):
-#
-#     global rate
-#
-#     print("Start publish")
-#
-#     print("Issue: 1")
-#     (trans,rot) = listener.lookupTransform('/cam_3_optical_frame', '/imu', rospy.Time(0))
-#     print("Issue: 2")
-#     msg = Odometry()
-#     msg.header.stamp = rospy.Time.now()
-#     msg.header.frame_id = "/map"#self.frame_id # i.e. '/odom'
-#     msg.child_frame_id = "/base_link"#self.child_frame_id # i.e. '/base_footprint'
-#     msg.pose.pose.position = Point(trans[0], trans[1], trans[2])
-#     msg.pose.pose.orientation = Quaternion(0.0, 0.0, 0.0, 1.0)
-#
-#     sevenSense_pub.publish(msg)
-#     print("End publish")
-#     rate.sleep()
-#
-# def run():
-#     global sevenSense_pub
-#     global rate
-#     rospy.loginfo("Transfromation starting up")
-#     rospy.init_node('Transfromation')
-#     sevenSense_pub = rospy.Publisher('/odom_sevenSense', Odometry)
-#
-#     listener = tf.TransformListener()
-#     rate = rospy.Rate(10)
-#     # rospy.Subscriber("/base_link_odom_camera_is1500", Odometry, turning_callback)
-#     publisher_sevenSense(sevenSense_pub, listener)
-#
-#
-#     # spin() simply keeps python from exiting until this node is stopped
-#     rospy.spin()
-
- #def main_loop(self):
-#    while not rospy.is_shutdown():
-#        self.rate.sleep()
 
 #*******************************************************************************
 #   Main
 #*******************************************************************************
 if __name__ == '__main__':
+    print("Start node: sevensense_tf_listener")
     rospy.init_node('sevensense_tf_listener')
-    print("Start")
+
+    # declarate and init. Publisher
     sevenSense_pub = rospy.Publisher('/odom_sevenSense', Odometry)
+    # declarate and init tf listener
     listener = tf.TransformListener()
 
+    # Rate for the ros-loop
     rate = rospy.Rate(10.0)
-    # print("Issue: 1")
+    # Loop
     while not rospy.is_shutdown():
         try:
+            # Get the lookupTransform to have the transfrom
             (trans,rot) = listener.lookupTransform('/base_link', '/map', rospy.Time(0))
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
             continue
-        # print(rot)
-        # print("Issue: 2")
+
+        # Create an odometry message
         msg = Odometry()
         msg.header.stamp = rospy.Time.now()
         msg.header.frame_id = "/map"#self.frame_id # i.e. '/odom'
@@ -107,8 +49,10 @@ if __name__ == '__main__':
         msg.pose.pose.position = Point(trans[0], trans[1], trans[2])
         msg.pose.pose.orientation = Quaternion(rot[0], rot[1], rot[2], rot[3])
 
+        # Publish the message
         sevenSense_pub.publish(msg)
 
         rate.sleep()
 
-    print("end")
+    print("End of the node")
+# end of the main
